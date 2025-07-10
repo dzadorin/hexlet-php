d32535ba37ba15409c728529e2873afc51c402d6
Zadorin Danil <zadorin7zadorin@yandex.ru>
2025-07-10 20:39:04 +0400

    work

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 
-echo 'HEXLET';
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Файл не включается напрямую
+// Он загрузится автоматически благодаря автозагрузке
+use Hexlet\Php\Runner;
+
+var_dump(Runner\run());
